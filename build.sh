067db051b06bc15d1398287292f171e14558ab1c
qlenlen <1783619611@qq.com>
2024-03-27 14:37:37 +0800

    Build menu: update

--- build.sh
+++ build.sh
@@ -3,7 +3,21 @@
 mkdir out
 mkdir aout
 
-make -j$(nproc) -C $(pwd) O=$(pwd)/out LLVM=1 LLVM_IAS=1 kalama_gki_defconfig
-make -j$(nproc) -C $(pwd) O=$(pwd)/out LLVM=1 LLVM_IAS=1
+KERNEL_DEFCONFIG=kalama_gki_defconfig
+
+CLANG_DIR="/home/tools/clang/17"
+export PATH="$CLANG_DIR/bin:$PATH"
+
+echo "**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****"
+echo -e "***************************************************"
+
+make -j$(nproc --all) -C $(pwd) O=$(pwd)/out \
+					  LLVM=1 \
+                      LLVM_IAS=1 \
+                      $KERNEL_DEFCONFIG
+ 
+make -j$(nproc --all) -C $(pwd) O=$(pwd)/out \
+					  LLVM=1 \
+                      LLVM_IAS=1 \
 
 cp out/arch/arm64/boot/Image $(pwd)/aout/Image
